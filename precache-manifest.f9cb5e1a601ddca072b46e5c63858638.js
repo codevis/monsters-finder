172af1129cdc9ccf24c0a099bc27a022556e89ee
codevis <visvis2k@gmail.com>
2020-10-11 13:15:19 +0530

    Updates

---
+++ precache-manifest.f9cb5e1a601ddca072b46e5c63858638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c54156a29f5ececf1480ef284ad0ae1",
+    "url": "/monsters-finder/index.html"
+  },
+  {
+    "revision": "c12aa6306fa1372e08d1",
+    "url": "/monsters-finder/static/css/main.62d0565b.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-finder/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-finder/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12aa6306fa1372e08d1",
+    "url": "/monsters-finder/static/js/main.7a9077ac.chunk.js"
+  },
+  {
+    "revision": "2c7cfb3f0e7d11517934",
+    "url": "/monsters-finder/static/js/runtime-main.88f13e48.js"
+  }
+]);
